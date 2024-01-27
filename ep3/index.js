8f56a19aab2a15acf0668f4afec97bafb1b41d38
Sarvesh207 <sarveshgaynar@gmail.com>
2024-01-27 10:29:31 +0530

    hoisting

---
+++ ep3/index.js
@@ -0,0 +1,16 @@
+//  console.log("Program start")
+//  getName();
+//  console.log(x);
+//     var x = 7;
+// function getName() {
+//    console.log("Namste js")
+// }
+
+console.log(a)
+var a = 7
+
+function a () {
+  console.log("I am function")
+}
+
+
