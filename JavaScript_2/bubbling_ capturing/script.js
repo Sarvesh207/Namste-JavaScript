46f416cb5745157ebbfdb184bb65bdeff9fbbab3
Sarvesh207 <sarveshgaynar@gmail.com>
2024-01-30 07:46:03 +0530

    bubbling and capturing

---
+++ JavaScript_2/bubbling_ capturing/script.js
@@ -0,0 +1,13 @@
+document.querySelector(".grandParent").addEventListener("click", (e) => {
+    console.log("GrandParent clicked")
+     e.stopPropagation();
+},true)
+
+document.querySelector(".parent").addEventListener("click", (e) => {
+    console.log("Parent clicked")
+   
+},true)
+
+document.querySelector(".child").addEventListener("click", (e) => {
+    console.log("Child clicked")
+},true)
