d10f37344ccf3c2cbc51e912d0d7612b6c6580d3
Sarvesh207 <sarveshgaynar@gmail.com>
2024-02-01 07:47:47 +0530

    event deligation

--- JavaScript_2/bubbling_ capturing/script.js
+++ JavaScript_2/bubbling_ capturing/script.js
@@ -1,13 +1,31 @@
-document.querySelector(".grandParent").addEventListener("click", (e) => {
-    console.log("GrandParent clicked")
-     e.stopPropagation();
-},true)
+const upperDiv = document.querySelector(".main")
+console.log(upperDiv)
+upperDiv.addEventListener("click", (e) => {
+    if(e.target.tagName === "BUTTON") {
 
-document.querySelector(".parent").addEventListener("click", (e) => {
-    console.log("Parent clicked")
+        console.log("button clicked")
+    }
+
+    console.log(e.target.tagName)
+
+})
+
+
+
+
+
+
+
+// document.querySelector(".grandParent").addEventListener("click", (e) => {
+//     console.log("GrandParent clicked")
+//      e.stopPropagation();
+// },true)
+
+// document.querySelector(".parent").addEventListener("click", (e) => {
+//     console.log("Parent clicked")
    
-},true)
+// },true)
 
-document.querySelector(".child").addEventListener("click", (e) => {
-    console.log("Child clicked")
-},true)
+// document.querySelector(".child").addEventListener("click", (e) => {
+//     console.log("Child clicked")
+// },true)
