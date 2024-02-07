13f99d3ffcc861d6cfd00be9662b7a05ba72d4a7
Sarvesh207 <sarveshgaynar@gmail.com>
2024-02-07 08:29:58 +0530

    Objects undetstantding

---
+++ JavaScript_2/Objects/index.js
@@ -0,0 +1,74 @@
+// const car = {
+//     firstname:"marl",
+//     lastname:"fortuner",
+//     manufacture:"2022"
+// }
+
+
+// function Person(firstname, lastname){
+//     this.firstname = firstname,
+//     this.lastname = lastname
+
+//     // return {firstname: "Jane", lastname: "Doe"}
+//     return [1, 2]
+// }
+
+// const p1 = new  Person("sarvesh", "gaynar")
+
+// console.log(p1);
+
+
+//  constructor fuctions 
+
+// function Person(firstname, lastname){
+//     return {
+//         firstname,
+//         lastname
+//     }
+// }
+
+
+// const p1 = Person("sarvesh", "gaynar")
+// console.log(p1)
+
+
+//  Object create
+
+// const person1 = {
+//     "name":"sarvesh"
+// }
+
+// const person2 = Object.create(person1)
+
+// console.log(person2.name)
+
+
+
+//  class
+
+class Person {
+    constructor(firstname, lastname){
+        this.firstname = firstname,
+        this.lastname = lastname
+    }
+
+    greet(){
+        console.log(`Hello I am ${this.firstname} ${this.lastname}`)
+    }
+}
+
+const p1 = new Person("sarvesh", "gaynar");
+
+console.log(p1)
+
+
+
+// different ways to crete objects in js 
+
+/**
+1) Object Literal
+2) Constructor Function
+3) Factory Functions 
+4) Object.Create Method
+5) Classes in JS
+ */
