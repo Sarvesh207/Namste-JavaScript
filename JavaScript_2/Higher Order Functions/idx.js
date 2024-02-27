6c24bc68c237a19ab3508ae025402b66b1fd4d34
Sarvesh207 <sarveshgaynar@gmail.com>
2024-02-27 23:51:10 +0530

    hof

--- JavaScript_2/Higher Order Functions/idx.js
+++ JavaScript_2/Higher Order Functions/idx.js
@@ -26,6 +26,12 @@ let circumference = function (radius) {
     return 2 * Math.PI * radius;
 };
 
+
+let diameter = function (radius){
+    return radius * 2;
+}
+
+
 let calculate = function (rediusArr, operation) {
     let output = [];
     for(let i=0; i<rediusArr.length; i++){
@@ -35,8 +41,29 @@ let calculate = function (rediusArr, operation) {
     return output;
 };
 
-console.log(calculate(arr, circumference))
-console.log(calculate(arr, area))
+// console.log(calculate(arr, circumference))
+// console.log(calculate(arr, area))
+// console.log(calculate(arr, diameter))
+
+
+// let dia = arr.map((num) => num*2)
+// console.log(dia);
+
+//  write custom map for calculate operations;
+
+Array.prototype.customMap = function (operation) {
+    let output = [];
+    for(let i=0; i<this.length; i++){
+        output.push(operation(this[i]))
+    }
+    
+    return output;
+};
+
+let result = arr.customMap((idx) => 5*idx)
+
+console.log(result);
+
 
 
 
